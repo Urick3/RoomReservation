0b7d46eb09a40e31f8d9e47081b316cebbd9ecc6
Igor Ubuntu <trabalho.computador.sjn@gmail.com>
2024-08-14 20:10:23 -0300

    feature: personalização da autenticação e criação do decorators para docente e gerente

--- sistema/users/models.py
+++ sistema/users/models.py
@@ -1,13 +1,28 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.db import models
 
-class CustomUser(AbstractUser):
+class User(AbstractUser):
     USER_TYPE_CHOICES = (
         ('manager', 'Gerente'),
         ('teacher', 'Docente'),
     )
     email = models.EmailField(unique=True)
-    type_user = models.CharField(max_length=10, choices=USER_TYPE_CHOICES)
+    user_type = models.CharField(max_length=10, choices=USER_TYPE_CHOICES, default='teacher')
+
+    groups = models.ManyToManyField(
+        Group,
+        related_name='custom_user_set',
+        blank=True,
+        help_text='The groups this user belongs to.',
+        verbose_name='groups',
+    )
+    user_permissions = models.ManyToManyField(
+        Permission,
+        related_name='custom_user_set',
+        blank=True,
+        help_text='Specific permissions for this user.',
+        verbose_name='user permissions',
+    )
 
     def __str__(self):
         return f"{self.username} ({self.get_type_user_display()})"
