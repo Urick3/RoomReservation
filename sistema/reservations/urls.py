f6103148c9b7bbe8dfcf91cbdb0140deddb841f7
Hadson00 <hadson798@gmail.com>
2024-08-15 22:16:59 -0300

    fix: alteração das urls e links nos templates

--- sistema/reservations/urls.py
+++ sistema/reservations/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('calendar/', calendar_teacher, name='calender'),
+    path('calendar/', calendar_teacher, name='calendar'),
     path('calendar/manager/', calendar_manager, name='calendar_manager'),
     path('pending/requests/', pending_requests, name='requests_pending'),
     path('total/request/', all_requests, name='total_request'),
--- sistema/reservations/views.py
+++ sistema/reservations/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect
-from rest_framework.response import Response
-from rest_framework.decorators import api_view
+# from rest_framework.response import Response
+# from rest_framework.decorators import api_view
 from .service import ReservationService
 from .models import Room
 
@@ -25,15 +25,15 @@ def requests(request):
 def hours(request):
     return render(request, 'reservations/hours.html')
 
-@login_required(login_url='/login/')
-@api_view(['GET'])
-def check_available_hours(request, room_id, date):
-    try:
-        room = Room.objects.get(id=room_id)
-        available_hours = ReservationService.list_available_hours_for_date(room, date)
-        return Response({
-            'available_hours': list(available_hours.values('id', 'range_hour'))
-        })
-    except Room.DoesNotExist:
-        return Response({'error': 'Room not found'}, status=404)
+# @login_required(login_url='/login/')
+# @api_view(['GET'])
+# def check_available_hours(request, room_id, date):
+#     try:
+#         room = Room.objects.get(id=room_id)
+#         available_hours = ReservationService.list_available_hours_for_date(room, date)
+#         return Response({
+#             'available_hours': list(available_hours.values('id', 'range_hour'))
+#         })
+#     except Room.DoesNotExist:
+#         return Response({'error': 'Room not found'}, status=404)
 
